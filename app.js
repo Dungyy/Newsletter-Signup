95d94c802a643173ea956accc8b7b35e2139aab3
Dungyy <erickmunoz13@gmail.com>
2021-11-15 22:22:29 -0600

    Set Up Server Connection

---
+++ app.js
@@ -0,0 +1,16 @@
+
+const express = require("express");
+const bodyPaser = require("body-parser");
+const get = require("request"); 
+
+const app = express();
+
+app.get('/', function(req, res){
+    res.send("The connection was a sucess.")
+});
+
+
+
+app.listen(4000, function(){
+    console.log("Server Running On 4000")
+});
